a2828f5a971d9c73406a2893f4da3ae2659f321c
urmit sardhara <sardharaurmit@gmail.com>
2026-05-15 18:50:09 +0530

    add temp dtl system

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from "react"
 
 function App() {
  const [name, setname] = useState("")
+ const [dtl, setdtl] = useState("")
+
   const Bus=(e)=>{
+
     e.preventDefault()
-   console.log(name);
+   console.log("task Heading is - ",name);
+   console.log("ditaisl is - ",dtl);
+   
+   setname("")
+   setdtl("")
    
   }
+
   return (
+
     <section  className="bg-black  h-screen" >
 
 
@@ -15,12 +24,13 @@ function App() {
         Bus(e)
         
       }}
+
        className="text-white flex justify-between pr-10" >
 
 
           <div className="flex w-1/2 items-start gap-3 ml-10 flex-col p-5 " >
 
-        <input className="px-5 w-full py-2 border-2 rounded" 
+        <input className="px-5 w-full py-2 border-2 rounded focus:outline-none" 
           type="text"
           placeholder="Enter Task Heading "
           value={name}
@@ -31,16 +41,20 @@ function App() {
 
           />
 
-          <input className="px-5 w-full py-2 border-2 rounded" 
-           type="texArea" 
+          <textarea className="px-5 w-full h-40  resize-none py-2 border-2 rounded focus:outline-none" 
+           type="text" 
            placeholder="Write Details"
+           value={dtl}
+           onChange={(e)=>{
+            setdtl(e.target.value)
+           }}
            />
          
            <button  className="text-black bg-white w-full py-2 px-5 rounded" > click me </button>
 
        
            </div>
-           {/* <img className="h-59" src="https://static.vecteezy.com/system/resources/thumbnails/073/034/096/small/cute-lavender-spiral-notebook-with-happy-face-illustration-free-png.png" alt="" /> */}
+          
       </form>
     </section>
     
