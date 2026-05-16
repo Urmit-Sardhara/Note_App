fe383b671e78a2b2757545b9ba50470b87fd1f76
urmit sardhara <sardharaurmit@gmail.com>
2026-05-16 11:43:19 +0530

    add final responsive desinge

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,14 @@ function App() {
 
   return (
 
-    <section  className="bg-black  h-screen lg:flex" >
-
+    <section  className="bg-black  h-screen lg:flex " >
 
       <form onSubmit={(e)=>{
         Bus(e)
         
       }}
-
-       className="text-white flex   flex-col gap-5  p-5 lg:w-1/2  " >
-
+       className="text-white flex   flex-col gap-5  lg:p-9 lg:w-1/2  " >
+        <h1 className="font-bold text-2xl pt-4 lg:p-0">Add Notes</h1>
         <input className="px-5 w-full py-2 border-2 rounded focus:outline-none" 
           type="text"
           placeholder="Enter Task Heading "
@@ -45,12 +43,22 @@ function App() {
            }}
            />
          
-           <button  className="text-black bg-white w-full py-2 px-5 rounded" > click me </button>
+           <button  className="text-black bg-white w-full py-2 px-5 rounded font-bold " > Add Note </button>
           
       </form>
-      <div className="lg:w-1/2 bg-red-400 p-5">
-       <div className="w-40 h-50 bg-amber-50 " >  </div></div>
+      <div className="lg:w-1/2 lg:border-l-2 border-white p-4 lg:p-10 h-full ">
+
+      <h1 className="font-bold text-2xl text-white">Recent Notes</h1>
+
+      <div className="flex gap-4 flex-wrap  h-full overflow-auto "> 
+        <div className="w-40 h-52 bg-amber-50 rounded-2xl p-2"></div>
+         
+       
+        
+      </div>
+      
      
+      </div>
     </section>
     
   )
