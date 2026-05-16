65add3f124ae19dda185338118662c3e4a92129b
urmit sardhara <sardharaurmit@gmail.com>
2026-05-16 10:31:26 +0530

    fix code readability

--- src/App.jsx
+++ src/App.jsx
@@ -27,9 +27,6 @@ function App() {
 
        className="text-white flex   flex-col gap-5  p-5 lg:w-1/2  " >
 
-
-          
-
         <input className="px-5 w-full py-2 border-2 rounded focus:outline-none" 
           type="text"
           placeholder="Enter Task Heading "
@@ -37,8 +34,6 @@ function App() {
         onChange={(e)=>{
           setname(e.target.value)
         }}
-        
-
           />
 
           <textarea className="px-5 w-full h-40  resize-none py-2 border-2 rounded focus:outline-none" 
@@ -51,9 +46,6 @@ function App() {
            />
          
            <button  className="text-black bg-white w-full py-2 px-5 rounded" > click me </button>
-
-       
-          
           
       </form>
       <div className="lg:w-1/2 bg-red-400 p-5">
